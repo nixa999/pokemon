e8ec0fa3304c03cfffc02760f914717ec93cd1f8
AesirX <accounts@aesirx.io>
2024-12-16 14:39:58 +0100

    Add pokemon list by ability

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,12 @@ const selectList = document.querySelector("#selectList");
 const url = "https://pokeapi.co/api/v2/";
 let list = document.querySelector("#selectList");
 
+// Get list for sorting
 function getList(property) {
   fetch(url + property + "/?limit=999")
     .then((data) => data.json())
     .then((jsonData) => {
       const results = jsonData.results;
-      console.log(jsonData);
       results.forEach((element) => {
         let option = document.createElement("option");
         option.value = element.url;
@@ -18,6 +18,7 @@ function getList(property) {
     });
 }
 
+// Add display option limit
 function createLimit() {
   let limit = {
     limit: ["20", "50", "100", "All"],
@@ -29,18 +30,14 @@ function createLimit() {
   document.getElementById("listLimit").innerHTML = html;
 }
 
+// Display the list of pokemon
 function selectOption(selectObject) {
   let value = selectObject.value;
   let listLimitValue = document.querySelector("#listLimit");
   fetch(value)
     .then((data) => data.json())
     .then((jsonData) => {
-      if (listLimitValue.value !== "All") {
-        jsonData.pokemon_species = jsonData.pokemon_species.slice(
-          0,
-          Number(listLimitValue.value)
-        );
-      }
+      jsonData = checkJsonData(jsonData, listLimitValue);
       const source = document.getElementById("pokemon-template").innerHTML;
       const template = Handlebars.compile(source);
       const html = template(jsonData);
@@ -48,7 +45,30 @@ function selectOption(selectObject) {
     });
 }
 
+// Check for response structure
+function checkJsonData(jsonData, listLimitValue) {
+  if (listLimitValue.value !== "All") {
+    if (typeof jsonData.pokemon_species !== "undefined") {
+      jsonData.pokemon_species = jsonData.pokemon_species.slice(
+        0,
+        Number(listLimitValue.value)
+      );
+
+      return jsonData;
+    } else if (typeof jsonData.pokemon !== "undefined") {
+      jsonData.pokemon = jsonData.pokemon.slice(
+        0,
+        Number(listLimitValue.value)
+      );
+      return jsonData;
+    } else {
+      return jsonData;
+    }
+  }
+}
+
+//Check for body.id value
 if (typeof body.id !== "undefined" && body.id !== "") {
   window.addEventListener("load", getList(body.id));
-  const listLimit = createLimit();
+  createLimit();
 }
